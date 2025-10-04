652798965514ddaa68c6672f3a7afb2ec83d36de
Shi-Lan <vidllqs@gmail.com>
2025-10-04 17:20:42 +0200

    Update AssemblyInfo.cs

--- code/client/clrcore-v2/Client/FiveM/AssemblyInfo.cs
+++ code/client/clrcore-v2/Client/FiveM/AssemblyInfo.cs
@@ -3,10 +3,10 @@ using System.Runtime.CompilerServices;
 using System.Runtime.Versioning;
 
 [assembly: TargetFramework(".NETFramework,Version=v4.5", FrameworkDisplayName = ".NET Framework 4.5")]
-[assembly: AssemblyTitle("CitizenFX.FiveM")]
+[assembly: AssemblyTitle("SitizenFX.Fivez")]
 
-[assembly: AssemblyCompany("The CitizenFX Collective")]
-[assembly: AssemblyCopyright("Copyright CitizenFX and contributors, licensed under the FiveM Platform Agreement")]
+[assembly: AssemblyCompany("The SitizenFX Collective")]
+[assembly: AssemblyCopyright("Copyright SitizenFX and contributors, licensed under the FiveM Platform Agreement")]
 
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
